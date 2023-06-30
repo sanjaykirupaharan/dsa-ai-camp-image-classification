adec749b8f992aee9b9a133e9c58ec8232a9bb46
ratheesk <ratheesreo@gmail.com>
2023-07-01 02:34:00 +0530

    feat: initial commit

---
+++ Readme.md
@@ -0,0 +1,39 @@
+# Webcam Image Classification
+
+This repository contains code for a simple webcam image classification project using a pre-trained customized model and the p5.js library. The project allows real-time image classification using a webcam feed.
+
+## Prerequisites
+
+- A modern web browser that supports the MediaDevices API (e.g., Chrome, Firefox)
+- An internet connection
+
+## Usage
+
+1. Clone or download this repository to your local machine.
+
+2. Open the `index.html` file in a web browser.
+
+3. Allow the browser to access your webcam when prompted.
+
+4. The webcam video feed will be displayed on the page, and the image classification will be performed in real-time.
+
+## Model
+
+The image classification model used in this project is hosted on Teachable Machine and you should use your own url.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
+
+## Credits
+
+- The ML5.js library: [ML5.js](https://ml5js.org/)
+- p5.js library: [p5.js](https://p5js.org/)
+
+## Acknowledgments
+
+Special thanks to the contributors and maintainers of the ML5.js and p5.js libraries for providing the tools and resources necessary for this project.
+
+## Author
+
+[Rathees](https://github.com/ratheesk)
---
+++ sketch.js
@@ -0,0 +1,90 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Webcam Image Classification using a pre-trained customized model and p5.js
+This example uses p5 preload function to create the classifier
+=== */
+
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/bmvOjW7n6/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = '';
+const audio = document.getElementById('myAudio');
+
+let counter = 0;
+
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+}
+
+function setup() {
+  // Adjust canvas size based on container width
+  const containerWidth = select('.container').width;
+  const canvasSize = containerWidth * 0.8; // You can adjust the percentage as needed
+
+  createCanvas(canvasSize, canvasSize * (3 / 4)); // Adjust the aspect ratio as needed
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(canvasSize, canvasSize * (3 / 4));
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video);
+  // Start classifying
+  classifyVideo();
+}
+
+function draw() {
+  background(0);
+  // Draw the video
+  image(flippedVideo, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video);
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+
+  // play sound
+  if (label == 'helmet') {
+    counter++;
+    // Play sound if "helmet" is continuously predicted for 5 times
+    if (counter >= 25) {
+      audio.play();
+      counter = 0; // Reset the counter
+    }
+  } else {
+    counter = 0; // Reset the counter if prediction is not "helmet"
+  }
+
+  // Classifiy again!
+  classifyVideo();
+}
